feb4dd9b43cb236e581ae40956690e03fd0c1a3b
KUMARI-SONALIUPADHYAY <upadhyaysonali2005@gmail.com>
2025-11-11 10:33:46 +0530

    Added solution for 11-08-2025 - KUMARI SONALI

--- Daily-Solutions/11-08-2025/KUMARISONALIUPADHYAY_6606175.java
+++ Daily-Solutions/11-08-2025/KUMARISONALIUPADHYAY_6606175.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
-//fibonacci Series
 public class KUMARISONALIUPADHYAY_6606175 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+    
         System.out.print("Enter the number: ");
-        int n = scan.nextInt();
+        int num = scan.nextInt();
         scan.close();
 
-        int result = fibonacci(n);
-        System.out.println("The " + n + "th Fibonacci number is: " + result);
+        int result = fibonacci(num);
+        System.out.println("The " + num + "th Fibonacci number is: " + result);
     }
 
-    public static int fibonacci(int n) {
-        if (n == 0) return 0;  // Base case F(0)
-        if (n == 1) return 1;  // Base case F(1)
+    public static int fibonacci(int num) {
+        if (num == 0) return 0;  // Base case F(0)
+        if (num == 1) return 1;  // Base case F(1)
 
         int a = 0;  // F(0)
         int b = 1;  // F(1)
         int c = 0;  // To store F(n)
 
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i <= num; i++) {
             c = a + b;  // F(n) = F(n-1) + F(n-2)
             a = b;      // Move forward
             b = c;
